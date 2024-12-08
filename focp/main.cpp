3cc42f4e57e5c283154149f6c45f7a3d6a1e6229
alapgohar <148534307+alapgohar@users.noreply.github.com>
2024-12-08 20:43:53 +0500

    finalized

--- focp/main.cpp
+++ focp/main.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include <limits>
 #include "Grid.h"             //for grid related functions like initialization, displaying and freeing
 #include "mine_layouts.h"     //for placement of mines
 #include "player_movements.h" //for player movements and mine detections
@@ -12,47 +13,76 @@ int count_adjacentMines(int player_row, int player_column, char **grid);
 
 int main()
 {
-    int answer;
+    char answer;
     char **grid;
     int rows, columns;
     srand(time(0));
 
     // main menu
-    cout << "Welcome to MINE MAZE" << endl
-         << "Please Select a difficulty level" << endl // getting the difficulty
-         << "1 for easy, 2 for hard" << endl
-         << "0 to quit" << endl;
+    cout << "***********************************************" << endl
+         << "*                                             *" << endl
+         << "*               Welcome to MINE MAZE          *" << endl
+         << "*                                             *" << endl
+         << "*        Please Select a difficulty level     *" << endl
+         << "*                                             *" << endl
+         << "*    E/e for Easy      |     H/h for Hard     *" << endl
+         << "*                  Q/q to Quit                *" << endl
+         << "*                                             *" << endl
+         << "***********************************************" << endl;
     cin >> answer;
 
-    while (!((answer == 0) || (answer == 1) || (answer == 2))) // input validation
+    //  validation for unexpected data types and extra characters
+    while (cin.fail() || (cin.peek() != '\n' && cin.peek() != EOF))
     {
-        cout << "Invalid Input. Please enter a number (0-2)";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input! Please enter a single character." << endl;
         cin >> answer;
     }
-    if (answer == 0) // quitting
+
+    // validating the input against allowed options
+    while (answer != 'E' && answer != 'e' && answer != 'H' && answer != 'h' && answer != 'Q' && answer != 'q')
     {
-        cout << "GoodBye!";
+        cout << "Invalid input! Please enter E/e, H/h, or Q/q: ";
+        cin >> answer;
+
+        //  validation for unexpected data types and extra characters
+        while (cin.fail() || (cin.peek() != '\n' && cin.peek() != EOF))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input! Please enter a single character." << endl;
+            cin >> answer;
+        }
+    }
+
+    if (answer == 'Q' || answer == 'q') // quitting
+    {
+        cout << "***********************************************" << endl
+             << "*                                             *" << endl
+             << "*          GoodBye! Quitting Now              *" << endl
+             << "*                                             *" << endl
+             << "***********************************************" << endl;
+
         return 0;
     }
-    else if (answer == 1) // easy level initialization
+    else if (answer == 'E' || answer == 'e') // easy level initialization
     {
         grid = initializeGrid(5, 5);
         placeMines(5, 5, 'e', grid);
         rows = 5, columns = 5;
-        // displayGrid(5, 5, grid);
     }
     else // hard level initialization
     {
         grid = initializeGrid(8, 8);
         placeMines(8, 8, 'h', grid);
         rows = 8, columns = 8;
-        // displayGrid(8, 8, grid);
     }
 
     // player movement
 
     char **displayed_Grid = initializeGrid(rows, columns); // the grid that will be shown to the player
-    displayed_Grid[0][0] = 'P';
+    displayed_Grid[0][0] = 'P';                            // initial position
 
     int player_row = 0, player_column = 0; // for keeping track of player positions
 
@@ -73,7 +103,15 @@ int main()
 
         if (player_row == rows - 1 && player_column == columns - 1) // checking for last cell
         {
-            cout << "Congratulations! You reached the goal." << endl;
+            cout << "***********************************************" << endl
+                 << "*                                             *" << endl
+                 << "*          Congratulations! You reached       *" << endl
+                 << "*                the goal.                    *" << endl
+                 << "*                                             *" << endl
+                 << "***********************************************" << endl;
+
+            cout << "Here is the grid: " << endl;
+            displayGrid(rows, columns, grid);
             break;
         }
 
--- focp/player_movements.cpp
+++ focp/player_movements.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "player_movements.h"
 
 using namespace std;
@@ -8,9 +9,9 @@ bool valid_move(int newRow, int newCol, int rows, int columns)
     return (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < columns); // checking if player is going beyond grid boundaries
 }
 
-void movement(int &player_row, int &player_column, int rows, int columns)
+void movement(int &player_row, int &player_column, int rows, int columns) // movemwnt of player
 {
-    int new_row = player_row, new_column = player_column;
+    int new_row = player_row, new_column = player_column; // temporary variables that will be validated firsyt
     char move;
 
     while (true)
@@ -19,27 +20,39 @@ void movement(int &player_row, int &player_column, int rows, int columns)
         cout << "Use W, A, S, D to move up, down, left, right: " << endl;
         cin >> move;
 
+        if (cin.fail() || cin.peek() != '\n') // input validation for unexpected data types and longer inputs
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input! Please enter a single character.\n";
+            continue;
+        }
+
         switch (move)
         {
         case 'w':
+        case 'W':
             new_row--;
             break;
         case 's':
+        case 'S':
             new_row++;
             break;
         case 'a':
+        case 'A':
             new_column--;
             break;
         case 'd':
+        case 'D':
             new_column++;
             break;
         default:
-            cout << "Invalid Input! use w/a/s/d" << endl;
+            cout << "Invalid Input! use w/a/s/d" << endl; // input validation
             continue;
         }
-        if (valid_move(new_row, new_column, rows, columns))
+        if (valid_move(new_row, new_column, rows, columns)) // checks the validity of move ie it is not crossing the boundary
         {
-            player_row = new_row;
+            player_row = new_row; // now updates the player positions
             player_column = new_column;
             break;
         }
